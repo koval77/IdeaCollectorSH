45a619212f068d9ad1dbc02e63f27325654c1ea8
koval77 <wojtek78@yahoo.com>
2020-03-24 22:43:19 +0000

    roles working

---
+++ Controllers/QAManagerController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdeaCollectorSH.Controllers
+{
+    public class QAManagerController : Controller
+    {
+        [Authorize(Roles ="QAManager")]
+        // GET: QAManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: QAManager/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: QAManager/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QAManager/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QAManager/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: QAManager/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QAManager/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: QAManager/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
